3bdd0ccd4b42a1846c04729109c0819638661485
murtazakp <khopoliwala52@gmail.com>
2024-03-15 17:06:46 +0530

    fix the error

--- src/app/components/corporateGovernance/corporateGovernance.tsx
+++ src/app/components/corporateGovernance/corporateGovernance.tsx
@@ -5,7 +5,7 @@ const CorporateGovernance = () => {
     <div className="flex flex-col gap-10 mt-9">
       <p>
         Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        industry. Lorem Ipsum has been the industrys standard dummy text ever
         since the 1500s, when an unknown printer took a galley of type and
         scrambled it to make a type specimen book. It has survived not only five
         centuries, but also the leap into electronic typesetting, remaining
@@ -22,11 +22,11 @@ const CorporateGovernance = () => {
         Latin words, consectetur, from a Lorem Ipsum passage, and going through
         the cites of the word in classical literature, discovered the
         undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33
-        of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by
+        of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by
         Cicero, written in 45 BC. This book is a treatise on the theory of
         ethics, very popular during the Renaissance. The first line of Lorem
-        Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section
-        1.10.32. The standard chunk of Lorem Ipsum used since the 150
+        Ipsum,Lorem ipsum dolor sit amet, comes from a line in section 1.10.32.
+        The standard chunk of Lorem Ipsum used since the 150
       </p>
     </div>
   );
