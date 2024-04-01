6bc7d648433956ab8ed3c5151a83e83852a2dbb2
Girish Boke <girish.boke@gmail.com>
2024-04-01 20:57:18 +0530

    typo error for 'permanent'

--- next.config.js
+++ next.config.js
@@ -4,72 +4,72 @@ module.exports = {
             {
                 source: '/services_hsc.html',
                 destination: '/services',
-                permanant: true
+                permanent: true
             },
             {
                 source: '/products_platform.html',
                 destination: '/software/mediqm-platform',
-                permanant: true
+                permanent: true
             },
             {
                 source: '/mymediqm_pa.html',
                 destination: '/partners',
-                permanant: true
+                permanent: true
             },
             {
                 source: '/services_mqx.html',
                 destination: '/services',
-                permanant: true
+                permanent: true
             },
             {
                 source: '/mymediqm_ca.html',
                 destination: '/careers',
-                permanant: true
+                permanent: true
             },
             {
                 source: '/about_mqx.html',
                 destination: '/about',
-                permanant: true
+                permanent: true
             },
             {
                 source: '/services_qmet.html',
                 destination: '/services/quality-management',
-                permanant: true
+                permanent: true
             },
             {
                 source: '/mymediqm.html',
                 destination: '/contact',
-                permanant: true
+                permanent: true
             },
             {
                 source: '/services_hqc.html',
                 destination: '/services/hospital-quality',
-                permanant: true
+                permanent: true
             },
             {
                 source: '/products_mqx.html',
                 destination: '/software/mediqm-platform',
-                permanant: true
+                permanent: true
             },
             {
                 source: '/about_cp.html',
                 destination: '/company-profile',
-                permanant: true
+                permanent: true
             },
             {
                 source: '/contact_mqx.html',
                 destination: '/contact',
-                permanant: true
+                permanent: true
             },
             {
                 source: '/services.html',
                 destination: '/services',
-                permanant: true
+                permanent: true
             },
             {
                 source: '/contact.html',
                 destination: '/contact',
-                permanant: true
+                permanent: true
             }
         ]
     }
